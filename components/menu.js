ba840d634411c4d3c7f96c7f004eefd377219a8d
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2022-03-23 14:40:07 +0000

    copied over alternatives and guidlines, added styling, menu-component

---
+++ components/menu.js
@@ -0,0 +1,88 @@
+class Menu extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		const lightMode = {
+			"--primary": "0, 0%, 9%",
+			"--background": "0, 0%, 89%",
+			"--highlight": "162deg 90% 19%",
+		};
+
+		const darkMode = {
+			"--primary": "0, 0%, 89%",
+			"--background": "0, 0%, 9%",
+			"--highlight": "162deg 90% 48%",
+		};
+
+		const setDarkMode = () => {
+			localStorage.setItem("color-mode", "dark");
+			Object.keys(darkMode).forEach((prop) =>
+				root.style.setProperty(prop, darkMode[prop])
+			);
+		};
+
+		const setLightMode = () => {
+			localStorage.setItem("color-mode", "light");
+			Object.keys(lightMode).forEach((prop) => {
+				root.style.setProperty(prop, lightMode[prop]);
+			});
+		};
+		const root = document.documentElement;
+
+		const activePage = this.getAttribute("activePage");
+		this.innerHTML = `
+    <div class="menu">
+    <div class='switch' id='switch' ></div>
+			<div class='wrapper'>
+				<h3>BUS</h3>
+				<p>Mental Health Support Chat</p>
+			</div>
+			<ul>
+				<li class="${activePage === "home" ? "active" : ""}">
+                <a href="/">Home</a>
+                </li>
+				<li class="${
+					activePage === "alternatives" ? "active" : ""
+				}"><a href="/pages/alternatives.html" >Self-harm alternatives</a></li>
+				<li  class="${
+					activePage === "guidelines" ? "active" : ""
+				}"><a href="/pages/guidelines.html">Guidelines</a></li>
+			</ul>
+		</div>
+    `;
+
+		const handleSwitch = () => {
+			const colorMode = localStorage.getItem("color-mode");
+			const switchBtn = document.getElementById("switch");
+
+			if (colorMode && colorMode === "dark") {
+				setLightMode();
+				switchBtn.classList.remove("on");
+			} else {
+				setDarkMode();
+				switchBtn.classList.add("on");
+			}
+		};
+		const switchBtn = document.getElementById("switch");
+		switchBtn.addEventListener("click", handleSwitch);
+
+		const colorMode = localStorage.getItem("color-mode");
+
+		const prefersDark =
+			(window.matchMedia &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches) ||
+			colorMode === "dark";
+
+		if (prefersDark) {
+			setDarkMode();
+			switchBtn.classList.add("on");
+		} else {
+			setLightMode();
+			switchBtn.classList.remove("on");
+		}
+	}
+}
+
+customElements.define("menu-component", Menu);
---
+++ main.js
@@ -0,0 +1,13 @@
+// color preference changes css variables
+// const root = document.documentElement;
+
+// localStorage.setItem("color-mode", "dark");
+// const colorMode = localStorage.getItem("color-mode");
+// console.log(root);
+// if (colorMode) {
+// 	if (colorMode === "dark") {
+// 		root.style.setProperty("--primary-color", "rgb(226, 226, 226)");
+// 	} else {
+// 		root.style.setProperty("--primary-color", "rgb(22, 22, 22)");
+// 	}
+// }
